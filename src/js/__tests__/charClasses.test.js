d97d5e81a2dd4b4af5172a6da8724cd3327102bf
iPadla <petrozavr@ya.ru>
2025-01-09 17:31:41 +0300

    100% test + coverage

--- src/js/__tests__/charClasses.test.js
+++ src/js/__tests__/charClasses.test.js
@@ -10,41 +10,52 @@ import {
 
 test.each([
   ["tooLongName", "Bowman"],
-  ["s", "Bowman", "Wrong name"],
-  [132, "Bowman", "Wrong name"],
+  ["s", "Bowman"],
+  [132, "Bowman"],
 ])("Name (>2 && <10), string.", (name, type) => {
-  expect(new Character(name, type)).toBe(new Error("Wrong name"));
+  expect(() => {
+    new Character(name, type);
+  }).toThrow();
 });
 
 test("only allowed char type is created", () => {
-  expect(new Character("ValidName", "Dragonborn")).toEqual(
-    new Error("Wrong type")
-  );
+  expect(() => {
+    new Character("ValidName", "Dragonborn");
+  }).toThrow();
 });
 
 test("leveling up 0hp", () => {
-  let сhar = new Bowman("ValidName", "Bowman");
-  сhar.damage(200);
-  expect(сhar.levelUp()).toEqual(
-    new Error("Dead don`t level up. Not sure what to do with an Undead tho...")
-  );
+  let char = new Bowman("ValidName", "Bowman");
+  char.damage(200);
+  expect(() => {
+    char.levelUp();
+  }).toThrow();
 });
 
 test("leveling up, level+1, attack and defence +20%, health 100", () => {
-  let сhar = new Bowman("ValidName", "Bowman");
-  expect(char.levelUp()).toEqual({
+  let char = new Bowman("ValidName", "Bowman");
+  char.levelUp();
+  expect(char).toEqual({
     name: "ValidName",
     type: "Bowman",
     health: 100,
     level: 2,
-    attack: 48,
-    defence: 12,
+    attack: 30,
+    defence: 30,
   });
 });
 
-test("taking damage", () => {
-  let сhar = new Bowman("ValidName", "Bowman");
-  expect(сhar.damage(25).health).toEqual(81.25);
+test("taking 25 damage to 25 def, should be 81.25", () => {
+  let char = new Bowman("ValidName", "Bowman");
+  char.damage(25);
+  expect(char.health).toEqual(81.25);
+});
+
+test("taking damage to 0 hp, should stay 0", () => {
+  let char = new Bowman("ValidName", "Bowman");
+  char.health = 0;
+  char.damage(25);
+  expect(char.health).toEqual(0);
 });
 
 test("Bowman created should have AttackDefence 25/25", () => {
